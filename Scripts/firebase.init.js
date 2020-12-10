546d5b86ca4e0275f20163a32e6d15990237f6f4
Abdul Rafay Khan <rafaykhan.work@gmail.com>
2020-12-10 20:41:46 +0500

    Starting new

---
+++ Scripts/firebase.init.js
@@ -0,0 +1,20 @@
+﻿// Your web app's Firebase configuration
+// var firebaseConfig = {
+//     apiKey: "AIzaSyBxhkOKc_tWaeeUSePIwitc2Tm-aqrLTww",
+//     authDomain: "chat-app-7da13.firebaseapp.com",
+//     databaseURL: "https://chat-app-7da13.firebaseio.com",
+//     projectId: "chat-app-7da13",
+//     storageBucket: "",
+//     messagingSenderId: "454878238535",
+//     appId: "1:454878238535:web:6fb06475d86f38e4"
+// };
+var firebaseConfig = {
+    apiKey: "AIzaSyDTo8Hfuz_MzJkRR3CZUkPWB-2k-vewwCg",
+    authDomain: "chat-app-b90ca.firebaseapp.com",
+    projectId: "chat-app-b90ca",
+    storageBucket: "chat-app-b90ca.appspot.com",
+    messagingSenderId: "860662564178",
+    appId: "1:860662564178:web:8914fed9636f9a8e35e68b"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
